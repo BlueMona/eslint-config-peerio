963065838e5b0400046f7a9de8f4c9a56a228f81
Anri Asaturov <anri82@gmail.com>
2017-11-18 23:39:30 +0200

    feat: react-less version

---
+++ index.base.js
@@ -0,0 +1,50 @@
+let OFF = 0,
+    WARN = 1,
+    ERROR = 2;
+
+module.exports = {
+    extends: [
+        'eslint-config-airbnb-base'
+    ].map(require.resolve),
+    plugins: [],
+    settings: {
+    },
+    rules: {
+        'arrow-parens': OFF,
+        'arrow-body-style': OFF,
+        indent: [ERROR, 4, { SwitchCase: 1, VariableDeclarator: 1 }],
+        'one-var-declaration-per-line': OFF,
+        'one-var': OFF,
+        'no-unused-vars': WARN,
+        'comma-dangle': [ERROR, 'never'],
+        'prefer-const': WARN,
+        'no-use-before-define': [ERROR, { functions: false, classes: true }],
+        'no-unused-expressions': [ERROR, { allowShortCircuit: true, allowTernary: true }],
+        'no-param-reassign': [ERROR, { props: false }],
+        'no-restricted-syntax': OFF,
+        'no-extend-native': OFF,
+        'no-console': OFF,
+        'no-continue': OFF,
+        'no-plusplus': OFF,
+        'no-bitwise': OFF,
+        'class-methods-use-this': OFF,
+        'guard-for-in': OFF,
+        'max-len': [WARN, 120],
+        quotes: [ERROR, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+        semi: [ERROR, 'always'],
+        'func-names': OFF,
+        'space-before-function-paren': [ERROR, {
+            anonymous: 'never',
+            named: 'never',
+            asyncArrow: 'always'
+        }],
+        'no-confusing-arrow': OFF,
+        'no-underscore-dangle': OFF
+    },
+    globals: {
+        describe: false,
+        before: false,
+        it: false,
+        expect: false
+    }
+};
--- index.js
+++ index.js
@@ -2,57 +2,19 @@ var OFF = 0,
     WARN = 1,
     ERROR = 2;
 
-module.exports = {
-    extends: [
-        'eslint-config-airbnb'
-    ].map(require.resolve),
-    plugins: [],
-    settings: {
-    },
-    rules: {
-        "arrow-parens": OFF,
-        "arrow-body-style": OFF,
-        "indent": [ERROR, 4, { "SwitchCase": 1, "VariableDeclarator": 1 }],
-        "one-var-declaration-per-line": OFF,
-        "one-var": OFF,
-        "no-unused-vars": WARN,
-        "comma-dangle": [ERROR, "never"],
-        "prefer-const": WARN,
-        "no-use-before-define": [ERROR, { "functions": false, "classes": true }],
-        "no-unused-expressions": [ERROR, { "allowShortCircuit": true, "allowTernary": true }],
-        "no-param-reassign": [ERROR, { "props": false }],
-        "no-restricted-syntax": OFF,
-        "no-extend-native": OFF,
-        "no-console": OFF,
-        "no-continue": OFF,
-        "no-plusplus": OFF,
-        "no-bitwise": OFF,
-        "class-methods-use-this": OFF,
-        "guard-for-in": OFF,
-        "max-len": [WARN, 120],
-        "quotes": [ERROR, "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
-        "semi": [ERROR, "always"],
-        "func-names": OFF,
-        "space-before-function-paren": [ERROR, {
-            "anonymous": "never",
-            "named": "never",
-            "asyncArrow": "always"
-        }],
-        "react/jsx-indent": [ERROR, 4],
-        "react/jsx-first-prop-new-line": OFF,
-        "react/jsx-closing-bracket-location": OFF,
-        "react/jsx-indent-props": OFF,
-        "react/jsx-max-props-per-line": OFF,
-        "jsx-a11y/no-noninteractive-element-interactions": OFF,
-        "react/forbid-prop-types": OFF,
-        "react/no-unused-prop-types": OFF,
-        "no-confusing-arrow": OFF,
-        "no-underscore-dangle": OFF,
-    },
-    globals: {
-        "describe": false,
-        "before": false,
-        "it": false,
-        "expect": false
-    }
-};
+const base = require('./index.base');
+
+base.extends.push(require.resolve('eslint-config-airbnb'));
+
+Object.assign(base.rules, {
+    "react/jsx-indent": [ERROR, 4],
+    "react/jsx-first-prop-new-line": OFF,
+    "react/jsx-closing-bracket-location": OFF,
+    "react/jsx-indent-props": OFF,
+    "react/jsx-max-props-per-line": OFF,
+    "jsx-a11y/no-noninteractive-element-interactions": OFF,
+    "react/forbid-prop-types": OFF,
+    "react/no-unused-prop-types": OFF
+});
+
+module.exports = base;
